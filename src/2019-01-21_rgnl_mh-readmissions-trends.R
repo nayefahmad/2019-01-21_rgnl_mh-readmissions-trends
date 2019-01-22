cc0933562021ea96ccdd333a45466d620f6af996
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-01-21 18:38:09 -0800

    plot trends

--- src/2019-01-21_rgnl_mh-readmissions-trends.R
+++ src/2019-01-21_rgnl_mh-readmissions-trends.R
@@ -10,15 +10,51 @@
 
 library(tidyverse)
 library(here)
+library(janitor)
 
 
-# read in data: 
+# read in data: -----------
 df1.readmit.rates <- read_csv(here("data",
                                    "2019-01-21_rgnl_readmisison-rates.csv")) %>% 
       mutate_if(is.character, 
-                as.factor)
+                as.factor) %>% 
+      clean_names() %>% 
+      mutate(period = paste0(fiscal_year, 
+                             "-", 
+                             quarter))
+
 
 str(df1.readmit.rates)
 summary(df1.readmit.rates)
+
+
+
+# plot trends: ----------
+p1.trends <- 
+      df1.readmit.rates %>% 
+      ggplot(aes(x = period, 
+                 y = readmission_rate, 
+                 group = entity)) + 
+      geom_line(aes(colour = entity, 
+                    group = entity)) + 
+      geom_point(aes(colour = entity)) + 
+      
+      facet_wrap(~entity) + 
+      
+      scale_x_discrete(breaks = df1.readmit.rates$period[seq(1, 25, 4)]) + 
+      
+      theme_minimal() + 
+      theme(axis.text.x = element_text(angle = 45, 
+                                       hjust = 1), 
+            panel.border = element_rect(colour = "grey80", 
+                                        fill = NA)); p1.trends 
+
+
+
+
+# todo: split into training and test tests. fit model on training, forecast
+# forward. See what the discrepancy is.
+
+
 
 
